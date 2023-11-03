7bd24f840840907932aebb6c45ea575f144a7813
shrutishukla14 <shrutiarunshukla14@gmail.com>
2023-11-03 21:18:44 +0530

    Blogify-Login-Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Blogs from './components/Blogs';
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Create from './components/Create';
+import Login from './components/Login';
+import Reg from './components/Reg';
+import AddBlogs from './components/AddBlogs';
+import ReadPage from './components/ReadPage';
+import ForgotPassword from './components/ForgotPwd';
+import UReadPage from './components/UReadPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Blogs/> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cr" element={<Create />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/reg" element={<Reg />} />
+        <Route path="/ab" element={<AddBlogs />} />
+        <Route path="/read/:title" element={<ReadPage />} />
+        <Route path="/uread" element={<UReadPage/>}/>
+        <Route path='/fp' element={<ForgotPassword/>}/>
+      </Routes>  
+      </BrowserRouter>
     </div>
   );
 }
