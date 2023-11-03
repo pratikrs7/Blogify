306018fd62826d59f6723d2a7f881612b3ccb827
shrutishukla14 <shrutiarunshukla14@gmail.com>
2023-11-03 21:38:50 +0530

    Add-Blog-Page

--- src/components/AddBlogs.js
+++ src/components/AddBlogs.js
@@ -1,9 +1,177 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import UserBlogs from "./UserBlogs";
+import { ref, onValue, remove } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import db from "./FbConfig";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const AddBlogs = () => {
-  return (
-    <div>AddBlogs</div>
-  )
-}
+  const [uid, setUid] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [isInitialRender, setIsInitialRender] = useState(true);
 
-export default AddBlogs
+  // const handlePrint = () => {
+  //   window.print();
+  // };
+  useEffect(() => {
+    const user = getAuth().currentUser;
+    if (!isInitialRender) {
+      toast("Double Click on any word to get meaning", {
+        position: "top-right",
+        autoClose: false,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } else {
+      setIsInitialRender(false);
+    }
+  }, [isInitialRender]);
+
+  const handleDoubleClick = async (e) => {
+    e.preventDefault();
+    const selectedWord = window
+      .getSelection()
+      .toString()
+      .replace(/\s+/g, " ")
+      .replace(/[\.\*\?;!()\+,\[:\]<>^_`\[\]{}~\\\/\"\'=]/g, "")
+      .trim();
+
+    if (selectedWord) {
+      try {
+        const response = await fetch(
+          `https://api.dictionaryapi.dev/api/v2/entries/en/${selectedWord}`
+        );
+        const data = await response.json();
+        console.log(data);
+
+        if (Array.isArray(data) && data.length > 0) {
+          const firstEntry = data[0];
+          const meanings = firstEntry.meanings;
+
+          if (Array.isArray(meanings) && meanings.length > 0) {
+            const firstMeaning = meanings[0].definitions[0].definition;
+            // alert(`Meaning of ${selectedWord}: ${firstMeaning}`);
+            toast(`Meaning of ${selectedWord}: ${firstMeaning}`, {
+              position: "top-right",
+              autoClose: false,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            });
+          } else {
+            // alert(`No meaning found for ${selectedWord}`);
+            toast(`No meaning found for ${selectedWord}`, {
+              position: "top-right",
+              autoClose: false,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            });
+          }
+        } else {
+          // alert(`No meaning found for ${selectedWord}`);
+          toast(`No meaning found for ${selectedWord}`, {
+            position: "top-right",
+            autoClose: false,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      } catch (error) {
+        // alert(`Error occurred while fetching meaning: ${error.message}`);
+        toast(`Error occurred while fetching meaning: ${error.message}`, {
+          position: "top-right",
+          autoClose: false,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    }
+  };
+  useEffect(() => {
+    const user = getAuth().currentUser;
+    if (user) {
+      setUid(user.uid);
+      const userRef = ref(db, "blogs/" + user.uid);
+      onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data)
+        if (data) {
+          const blogsList = Object.entries(data).map(([key, value]) => ({
+            id: key,
+            title: value.title,
+            content: value.content,
+            iurl: value.iurl,
+            rurl: value.rurl,
+          }));
+          setBlogs(blogsList);
+        }
+      });
+    }
+  }, []);
+
+  if (blogs.length === 0) {
+    return (
+      <div>
+        <NavBar />
+        <h1>You haven't added any Blog yet.</h1>
+        <Link to="/cr">Create Blog</Link>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <ToastContainer
+        position="top-right"
+        autoClose={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="dark"
+      />
+      <div onDoubleClick={handleDoubleClick}>
+
+        
+      {blogs.map((a) => (
+        <>
+          <UserBlogs
+            
+            iurl={a.iurl}
+            title={a.title}
+            content={a.content}
+            rurl={a.rurl}
+          />
+        </>
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default AddBlogs;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,150 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import db from "./FbConfig";
+import { ref, set } from "firebase/database";
+import logo from "../assets/1024.png";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [uid, setUid] = useState("");
+  const [pwd, setPwd] = useState("");
 
-export default Login
+  const hEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const hPwd = (event) => {
+    setPwd(event.target.value);
+  };
+
+  const nav = useNavigate();
+
+  const login = (event) => {
+    event.preventDefault();
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, pwd)
+      .then((res) => {
+        // alert("Login Successful");
+        nav("/blogs", {state:{loginSucess: true}});
+        let n = res.user.uid;
+        console.log(n);
+        localStorage.setItem('uid', n);
+        setUid(n);
+      })
+      .catch((err) => alert("Invalid Credentials"));
+  };
+
+  const googleProvider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const googleSignin = () => {
+    signInWithPopup(auth, googleProvider).then((response) => {
+      console.log(response.user);
+      nav("/blogs");
+      let n = response.user.uid;
+      let email = response.user.email;
+      let r = ref(db, "registrations/" + n);
+      set(r, email);
+      console.log(n);
+      setUid(n);
+    });
+  };
+
+  const githubProvider = new GithubAuthProvider();
+
+  const githubSignin = () => {
+    signInWithPopup(auth, githubProvider).then((response) => {
+      console.log(response.user);
+      nav("/blogs");
+      let n = response.user.uid;
+      let email = response.user.email;
+      let r = ref(db, "registrations/" + n);
+      set(r, email);
+      console.log(n);
+      setUid(n);
+    });
+  };
+
+  return (
+    <div>
+      <div className="text-center">
+        <img
+          src={logo}
+          className="rounded"
+          alt="..."
+          style={{ height: "200px" }}
+        />
+      </div>
+      <form id="login">
+        <h1>Login</h1>
+        <br />
+        <div className="form-outline mb-4">
+          <input
+            type="email"
+            id="form2Example1"
+            className="form-control"
+            placeholder="EmailID"
+            onChange={hEmail}
+          />
+        </div>
+
+        <div className="form-outline mb-4">
+          <input
+            type="password"
+            id="form2Example2"
+            className="form-control"
+            placeholder="Password"
+            onChange={hPwd}
+          />
+        </div>
+
+        <div className="row mb-4">
+          <div className="col">
+            <Link to={"/fp"}>Forgot password?</Link>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="btn btn-primary btn-block mb-4"
+          value="Login"
+          onClick={login}
+        >
+          Sign in
+        </button>
+
+        <div className="text-center">
+          <p>
+            Not a member? <Link to={"/reg"}>Register</Link>
+          </p>
+          <p>or sign up with:</p>
+
+          <button
+            type="button"
+            className="btn btn-link btn-floating mx-1 btn-lg"
+            onClick={googleSignin}
+          >
+            <FontAwesomeIcon icon={faGoogle} />
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-link btn-floating mx-1 btn-lg"
+            onClick={githubSignin}
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
